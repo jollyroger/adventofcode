334046bca812d441541b3566adb3fa2f2df73817
Andrii Senkovych <andrii@senkovych.com>
2018-12-10 14:17:52 +0200

    Day 10 solved.

---
+++ 2018/10/10.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+from collections import defaultdict
+import re
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.points = ["position=< 9,  1> velocity=< 0,  2>",
+                       "position=< 7,  0> velocity=<-1,  0>",
+                       "position=< 3, -2> velocity=<-1,  1>",
+                       "position=< 6, 10> velocity=<-2, -1>",
+                       "position=< 2, -4> velocity=< 2,  2>",
+                       "position=<-6, 10> velocity=< 2, -2>",
+                       "position=< 1,  8> velocity=< 1, -1>",
+                       "position=< 1,  7> velocity=< 1,  0>",
+                       "position=<-3, 11> velocity=< 1, -2>",
+                       "position=< 7,  6> velocity=<-1, -1>",
+                       "position=<-2,  3> velocity=< 1,  0>",
+                       "position=<-4,  3> velocity=< 2,  0>",
+                       "position=<10, -3> velocity=<-1,  1>",
+                       "position=< 5, 11> velocity=< 1, -2>",
+                       "position=< 4,  7> velocity=< 0, -1>",
+                       "position=< 8, -2> velocity=< 0,  1>",
+                       "position=<15,  0> velocity=<-2,  0>",
+                       "position=< 1,  6> velocity=< 1,  0>",
+                       "position=< 8,  9> velocity=< 0, -1>",
+                       "position=< 3,  3> velocity=<-1,  1>",
+                       "position=< 0,  5> velocity=< 0, -1>",
+                       "position=<-2,  2> velocity=< 2,  0>",
+                       "position=< 5, -2> velocity=< 1,  2>",
+                       "position=< 1,  4> velocity=< 2,  1>",
+                       "position=<-2,  7> velocity=< 2, -2>",
+                       "position=< 3,  6> velocity=<-1, -1>",
+                       "position=< 5,  0> velocity=< 1,  0>",
+                       "position=<-6,  0> velocity=< 2,  0>",
+                       "position=< 5,  9> velocity=< 1, -2>",
+                       "position=<14,  7> velocity=<-2,  0>",
+                       "position=<-3,  6> velocity=< 2, -1>"]
+        self.steps = 3
+        self.sign = "\n".join(["............",
+                               ".#...#..###.",
+                               ".#...#...#..",
+                               ".#...#...#..",
+                               ".#####...#..",
+                               ".#...#...#..",
+                               ".#...#...#..",
+                               ".#...#...#..",
+                               ".#...#..###.",
+                               "............"])
+
+    def test(self):
+        data = parse_input("\n".join(self.points))
+        simulation = Simulation(data)
+        solve(simulation)
+        self.assertEqual(simulation.ts , 3)
+        self.assertEqual(simulation.__str__(), self.sign)
+
+def parse_input(s):
+    steps = []
+    expr = re.compile(r'-?\d+')
+    records = s.strip().split("\n")
+
+    for record in records:
+        m = tuple(int(i) for i in expr.findall(record))
+        if m:
+            steps.append(m)
+
+    return steps
+
+def get_input(filename='input'):
+    steps = []
+    expr = re.compile(r'-?\d+')
+    return parse_input(open(filename, 'r').read())
+
+class Simulation(object):
+    def __init__(self, start_state):
+        self.state = list(start_state)
+        self.ts = 0 # timestamp
+
+    def step(self, steps=1):
+        self.ts += steps
+        for i, p in enumerate(self.state):
+            x, y, dx, dy = p
+            self.state[i] = (x + dx * steps, y + dy * steps, dx, dy)
+
+    def to_time(self, ts):
+        steps = ts - self.ts
+        self.step(steps)
+
+    def region(self):
+        x_list, y_list, dx_list, dy_list = [i for i in zip(*self.state)]
+        return (min(x_list), min(y_list), max(x_list), max(y_list))
+
+    def __str__(self):
+        x1, y1, x2, y2 = self.region()
+        points = {(x,y):set() for x,y,_,_ in self.state}
+        result = ""
+
+        for y in range(y1-1, y2+2):
+            for x in range(x1-1, x2+2):
+                char = "#" if (x, y) in points else "."
+                result += char
+            result += "\n"
+
+        return result[:-1]
+
+
+def solve(simulation):
+    region = simulation.region()
+    area = abs(region[2] - region[0]) * abs(region[3] - region[1])
+    new_area = area
+    while True:
+        simulation.step()
+        region = simulation.region()
+        new_area = abs(region[2] - region[0]) * abs(region[3] - region[1])
+        if new_area > area:
+            simulation.step(-1)
+            break
+        area = new_area
+    return
+
+
+def main(argv):
+    data = get_input(argv[1])
+    simulation = Simulation(data)
+    print("Task 01")
+    solve(simulation)
+    print(simulation)
+    print("Task 02")
+    print("Found minimum at {} seconds".format(simulation.ts))
+
+
+if __name__ == '__main__':
+    import sys
+    unittest.main(argv=sys.argv[:1], exit=False, verbosity=0)
+    raw_input()
+    main(sys.argv)
